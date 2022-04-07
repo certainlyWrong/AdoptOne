03f38b9fc244813bc4046045917d55c49a8f1bc9
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2022-04-07 13:29:13 -0300

    :white_check_mark: Home: navbar responsive and animetes #15
    Parceria com Vinicius

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -10,13 +10,11 @@ window.addEventListener("scroll", () => {
     }
     comp = window.scrollY;
 });
-window.addEventListener("resize", () => {
-    let login = document.querySelector(".login-static");
-    // login.classList.toggle("deactivate-login", window.innerWidth <= 900);
-});
 button_menu_mobile.addEventListener("click", () => {
-    let menu = document.querySelector("#menu"), login = document.querySelector(".login-static");
+    console.log('asdasd');
+    let menu = document.querySelector("#menu"), login = document.querySelector(".login-static"), mobile_menu = document.querySelector(".mobile-menu");
     menu.classList.toggle("activate-menu", activate);
     login.classList.toggle("activate-login", activate);
+    mobile_menu.classList.toggle("active", activate);
     activate = !activate;
 });
--- src/scripts/navbar.ts
+++ src/scripts/navbar.ts
@@ -3,7 +3,6 @@ let comp: number = 0,
   button_menu_mobile = document.querySelector('#button-menu-mobile')!,
   activate = true;
 
-
 window.addEventListener("scroll", () => {
   let header = document.querySelector("header")!;
 
@@ -17,16 +16,19 @@ window.addEventListener("scroll", () => {
   comp = window.scrollY;
 });
 
-window.addEventListener("resize", () => {
-  let login = document.querySelector(".login-static")!;
-  // login.classList.toggle("deactivate-login", window.innerWidth <= 900);
-});
 
 button_menu_mobile.addEventListener("click", () => {
+
+  console.log('asdasd')
+
   let menu = document.querySelector("#menu")!,
-      login = document.querySelector(".login-static")!;
+      login = document.querySelector(".login-static")!,
+      mobile_menu = document.querySelector(".mobile-menu")!;
+
   menu.classList.toggle("activate-menu", activate);
   login.classList.toggle("activate-login", activate);
+  mobile_menu.classList.toggle("active", activate);
+
   activate = !activate;
 });
 

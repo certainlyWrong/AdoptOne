37c9fe393de520e67ea62b674060d2dd49bcf9d4
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2022-04-07 12:49:21 -0300

    :lipstick: Home: update navbar menu responsive #14

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -10,8 +10,12 @@ window.addEventListener("scroll", () => {
     }
     comp = window.scrollY;
 });
+window.addEventListener("resize", () => {
+    let login = document.querySelector(".login-static");
+    // login.classList.toggle("deactivate-login", window.innerWidth <= 900);
+});
 button_menu_mobile.addEventListener("click", () => {
-    let menu = document.querySelector("#menu"), login = document.querySelector(".login");
+    let menu = document.querySelector("#menu"), login = document.querySelector(".login-static");
     menu.classList.toggle("activate-menu", activate);
     login.classList.toggle("activate-login", activate);
     activate = !activate;
--- src/scripts/navbar.ts
+++ src/scripts/navbar.ts
@@ -1,14 +1,14 @@
 
 let comp: number = 0,
-    button_menu_mobile = document.querySelector('#button-menu-mobile')!,
-    activate = true;
+  button_menu_mobile = document.querySelector('#button-menu-mobile')!,
+  activate = true;
 
 
 window.addEventListener("scroll", () => {
   let header = document.querySelector("header")!;
 
   if (!activate) {
-    header.classList.toggle("sticky", true);    
+    header.classList.toggle("sticky", true);
   }
   else {
     header.classList.toggle("sticky", window.scrollY < comp || window.scrollY < 90);
@@ -17,10 +17,14 @@ window.addEventListener("scroll", () => {
   comp = window.scrollY;
 });
 
+window.addEventListener("resize", () => {
+  let login = document.querySelector(".login-static")!;
+  // login.classList.toggle("deactivate-login", window.innerWidth <= 900);
+});
 
 button_menu_mobile.addEventListener("click", () => {
   let menu = document.querySelector("#menu")!,
-    login = document.querySelector(".login")!;
+      login = document.querySelector(".login-static")!;
   menu.classList.toggle("activate-menu", activate);
   login.classList.toggle("activate-login", activate);
   activate = !activate;
